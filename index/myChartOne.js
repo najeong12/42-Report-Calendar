9a4a45f884ddc93da968c9ef93883acadb24e7be
이나정 <leena9901@gmail.com>
2021-02-07 20:04:36 +0900

    finish chart

---
+++ index/myChartOne.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('myChartOne').getContext('2d');
+    var myChartOne = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['1월 1주차', '1월 2주차', '1월 3주차', '1월 4주차', '2월 1주차', '2월 2주차'],
+            datasets: [{
+                // label: '주별 공부시간',
+                data: [35, 60, 40, 60, 100, 70],
+                backgroundColor: [
+                    'rgba(0, 185, 186, 0.3)',
+                    'rgba(0, 185, 186, 0.3)',
+                    'rgba(0, 185, 186, 0.3)',
+                    'rgba(0, 185, 186, 0.3)',
+                    'rgba(0, 185, 186, 0.3)',
+                    'rgba(0, 185, 186, 0.3)'
+                ],
+                borderColor: 'rgba(0, 185, 186, 1)',
+                borderWidth: 2
+                
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: '주차별 공부시간',
+                fontSize: 20,
+                fontStyle: 'bold'
+            },
+            legend: {
+                display: false
+            },
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
+});
+
---
+++ index/myChartTwo.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('myChartTwo').getContext('2d');
+    var myChartTwo = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Zoom', 'Slack', 'Discord', '기타'], 
+            datasets: [{
+                data: [40, 40, 15, 5],
+                backgroundColor: [
+                    'rgba(227, 89, 132, 0.7)',
+                    'rgba(60, 149, 254, 0.7)',
+                    'rgba(56, 234, 100, 0.7)',
+                    'rgba(231, 227, 32, 0.7)'
+                ],
+                label: 'Dataset 1',
+                borderColor: 'rgba(255, 255, 255, 1)',
+                borderWidth: 0,
+                hoverBorderColor: 'rgba(0, 185, 186, 1)',
+                hoverBorderWidth: 3,
+            }]
+        },
+        options: {
+            responsive: true, 
+            legend: false,
+            maintainAspectRatio : false, 
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            },
+            title: {
+                display: true,
+                text: '동료학습 방법',
+                fontSize: 20,
+                fontStyle: 'bold'
+            },
+            legend: {
+                display: true,
+                position: 'bottom'
+            }
+        }
+    });
+});
+
+/*
+ ,
+            pieceLabel: { 
+                mode:'label',
+                position:'outside',
+                fontSize: 11, 
+                fontStyle: 'bold',
+            },
+*/
